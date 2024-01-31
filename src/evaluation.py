c7054d94d28f1e479a0b6cf95d737163b0751775
loganriggs <logan.smith.5@gmail.com>
2024-01-31 20:30:10 +0000

    reformatted wandb, added split CE metrics, dead feature, l2 ratio

--- src/evaluation.py
+++ src/evaluation.py
@@ -20,7 +20,13 @@ def evaluate(
     ):
     
     # metrics = {k: [] for k in ["Loss Recovered", "FVU", "L0", "L1", "MSE", "Dead Features", "Feature Frequency", "Feature Magnitude"]}
-    metrics = {k: [] for k in ["FVU", "L0", "L1", "MSE", "Dead Features", "Feature Frequency"]}
+    metrics = {k: [] for k in [
+        "Metrics/CE Recovered", "Metrics/CE", "Metrics/CE Zero Ablated", "Metrics/CE SAE", "Metrics/CE-diff", "Metrics/FVU", "Metrics/L0",  "Metrics/L2 Norm (original activations)", "Metrics/L2 Ratio (reconstructed/original)",
+
+        "Losses/L1", "Losses/MSE", 
+        
+        "Sparsity/Dead Features", "Sparsity/Below 1e-5", "Sparsity/Below 1e-3", "Sparsity/Below 1e-2", "Sparsity/Below 1e-1", "Sparsity/Feature Frequency",
+        ]}
     for idx, batch in enumerate(tqdm(data_loader)):
         if(idx == 20):
             break
@@ -44,13 +50,29 @@ def evaluate(
                 reconstructions = dictionary.decode(features)
 
                 # compute metrics
-                # metrics["Loss Recovered"].append(FLR(activation_name, dictionary, input_ids, model))
-                metrics["FVU"].append(FVU(activations, reconstructions))
-                metrics["L0"].append(L0(features))
-                metrics["L1"].append(L1(features))
-                metrics["MSE"].append(MSE(activations, reconstructions))
-                metrics["Dead Features"].append(dead_features(feature_buffer))
-                metrics["Feature Frequency"].append(feature_frequency(feature_buffer))
+                flr, loss, loss_zero_ablation, loss_dict_reconstruction, ce_diff = FLR(activation_name, dictionary, input_ids, model)
+                l2_norm_original = torch.norm(activations, 2, dim=-1).mean()
+                l2_norm_reconstructed = torch.norm(reconstructions, 2, dim=-1).mean()
+                l2_ratio = l2_norm_reconstructed / l2_norm_original
+                metrics["Losses/L1"].append(L1(features))
+                metrics["Losses/MSE"].append(MSE(activations, reconstructions))
+
+                metrics["Metrics/CE Recovered"].append(flr)
+                metrics["Metrics/CE"].append(loss)
+                metrics["Metrics/CE Zero Ablated"].append(loss_zero_ablation)
+                metrics["Metrics/CE SAE"].append(loss_dict_reconstruction)
+                metrics["Metrics/CE-diff"].append(ce_diff)
+                metrics["Metrics/FVU"].append(FVU(activations, reconstructions))
+                metrics["Metrics/L0"].append(L0(features))
+                metrics["Metrics/L2 Norm (original activations)"].append(l2_norm_original)
+                metrics["Metrics/L2 Ratio (reconstructed/original)"].append(l2_ratio)
+
+                metrics["Sparsity/Dead Features"].append(dead_features(feature_buffer))
+                metrics["Sparsity/Below 1e-5"].append(dead_features(feature_buffer, threshold=1e-5))
+                metrics["Sparsity/Below 1e-3"].append(dead_features(feature_buffer, threshold=1e-3))
+                metrics["Sparsity/Below 1e-2"].append(dead_features(feature_buffer, threshold=1e-2))
+                metrics["Sparsity/Below 1e-1"].append(dead_features(feature_buffer, threshold=1e-1))
+                metrics["Sparsity/Feature Frequency"].append(feature_frequency(feature_buffer))
                 # metrics["Feature Magnitude"].append(feature_magnitude(feature_buffer))
 
     # compute average of each metric
