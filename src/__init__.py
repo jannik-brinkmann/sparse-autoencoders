7608bb16799325d56e02f0e751cdd8608bc89b63
Jannik Brinkmann <jannik.brinkmann698@gmail.com>
2024-02-13 12:17:16 +0100

    some refactoring and bug fixes

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,4 @@
 from .autoencoders import UntiedSAE
 from .training.cache import FeatureCache
 from .evaluation import evaluate
-from .utils import get_activation_path, get_activation_size, get_dataloader
 from .training import TrainingConfig, Trainer, CachedActivationLoader
--- src/training/dataloader.py
+++ src/training/dataloader.py
@@ -10,8 +10,6 @@ from baukit import Trace, TraceDict
 
 
 from .config import TrainingConfig
-from .data_utils import get_dataloader, cache_activations
-
 from .data_utils import chunk_and_tokenize, load_dataset
 
 class ActivationLoader(ABC):
@@ -35,16 +33,25 @@ class CachedActivationLoader(ActivationLoader):
         super().__init__(config)
         self.cache_dir = config.cache_dir
         
+        # check if data has already been cached before
+        exists = False
+        substring = f"{self.config.model_name_or_path}_{self.config.dataset_name_or_path}".replace("/", "_")
+        for root, dirnames, filenames in os.walk(self.config.cache_dir):
+            for filename in filenames:
+                if substring in filename:
+                    exists = True
+        
         # cache relevant activations
-        device = "cuda" if torch.cuda.is_available() else "cpu"
-        model = AutoModelForCausalLM.from_pretrained(config.model_name_or_path).to(device)
-        tokenizer = AutoTokenizer.from_pretrained(config.model_name_or_path)
-        self.train_loader, self.test_loader = get_dataloader(
-            config.dataset_name_or_path, 
-            tokenizer, 
-            config.batch_size, 
-            config.context_length)
-        cache_activations(config, model, self.train_loader, [config.hook_point], device)
+        if not exists:
+            device = "cuda" if torch.cuda.is_available() else "cpu"
+            model = AutoModelForCausalLM.from_pretrained(config.model_name_or_path).to(device)
+            tokenizer = AutoTokenizer.from_pretrained(config.model_name_or_path)
+            self.train_loader, self.test_loader = self.get_dataloader(
+                config.dataset_name_or_path, 
+                tokenizer, 
+                config.batch_size, 
+                config.ctx_length)
+            self.cache_activations(model, self.train_loader, [config.hook_point], device)
         
     def get_dataloader(self, dataset_name_or_path, tokenizer, batch_size, context_length):
         dataset = load_dataset(dataset_name_or_path, split="train")
@@ -58,19 +65,13 @@ class CachedActivationLoader(ActivationLoader):
         test_loader = DataLoader(tokenized_dataset["test"], batch_size=batch_size, shuffle=False)
         return train_loader, test_loader
     
-    def get_activation_path(self, args, idx):
-        return f"{self.config.model_name_or_path}_{self.config.dataset_name_or_path}_{self.config.hook_point}_{idx}.pt".replace("/", "_")
+    def get_activation_path(self, idx):
+        filename = f"{self.config.model_name_or_path}_{self.config.dataset_name_or_path}_{self.config.hook_point}_{idx}.pt".replace("/", "_")
+        filepath = os.path.join(self.config.cache_dir, filename)
+        return filepath
         
-    def cache_activations(args, model, data_loader, activation_names, device, check_if_exists=True):
-        os.makedirs(args.cache_dir, exist_ok=True)
-
-        # check if data has already been cached before
-        if check_if_exists:
-            substring = f"{args.model_name_or_path}_{args.dataset_name_or_path}".replace("/", "_")
-            for root, dirnames, filenames in os.walk(args.cache_dir):
-                for filename in filenames:
-                    if substring in filename:
-                        return
+    def cache_activations(self, model, data_loader, activation_names, device, check_if_exists=True):
+        os.makedirs(self.config.cache_dir, exist_ok=True)
 
         # go through the data_loader and save activations batch by batch
         for batch_idx, batch in enumerate(tqdm(data_loader)):
@@ -79,7 +80,7 @@ class CachedActivationLoader(ActivationLoader):
                 with TraceDict(model, activation_names) as ret:
                     _ = model(tokens)
                     for act_name in activation_names:
-                        activation_path = self.get_activation_path(args, act_name, batch_idx)
+                        activation_path = self.get_activation_path(batch_idx)
                         # if not os.path.isfile(activation_path):
                         representation = ret[act_name].output
                         if(isinstance(representation, tuple)):
@@ -104,7 +105,6 @@ class CachedActivationLoader(ActivationLoader):
         return len(self.train_loader)
         
     def __getitem__(self, idx):
-        file_name = self.get_activation_path(idx)
-        file_path = os.path.join(self.cache_dir, file_name)
-        activations = torch.load(file_path)
+        filepath = self.get_activation_path(idx)
+        activations = torch.load(filepath)
         return activations
