aa8b72cd9c52af88b925d20729f45848b843de5f
loganriggs <logan.smith.5@gmail.com>
2024-02-21 15:39:30 +0000

    diff dead feature counting method

--- training.py
+++ training.py
@@ -117,24 +117,24 @@ class ConstrainedAdamW(torch.optim.Adam):
         with torch.no_grad():
             p = self.constrained_param
             ## Joseph implementation
-            parallel_component = einops.einsum(
-                p.grad,    
-                p.data,
-                # "d_sae d_in, d_sae d_in -> d_sae",
-                "d_in d_sae , d_in d_sae  -> d_sae",
-            )
+            # parallel_component = einops.einsum(
+            #     p.grad,    
+            #     p.data,
+            #     # "d_sae d_in, d_sae d_in -> d_sae",
+            #     "d_in d_sae , d_in d_sae  -> d_sae",
+            # )
 
-            p.grad -= einops.einsum(
-                parallel_component,
-                p.data,
-                # "d_sae, d_sae d_in -> d_sae d_in",
-                "d_sae, d_in d_sae  ->  d_in d_sae",
-            )
+            # p.grad -= einops.einsum(
+            #     parallel_component,
+            #     p.data,
+            #     # "d_sae, d_sae d_in -> d_sae d_in",
+            #     "d_sae, d_in d_sae  ->  d_in d_sae",
+            # )
         
             # # Sam Marks implementation
-            # normed_p = p / p.norm(dim=0, keepdim=True)
-            # # project away the parallel component of the gradient
-            # p.grad -= (p.grad * normed_p).sum(dim=0, keepdim=True) * normed_p
+            normed_p = p / p.norm(dim=0, keepdim=True)
+            # project away the parallel component of the gradient
+            p.grad -= (p.grad * normed_p).sum(dim=0, keepdim=True) * normed_p
         super().step(closure=closure)
         with torch.no_grad():
             p = self.constrained_param
