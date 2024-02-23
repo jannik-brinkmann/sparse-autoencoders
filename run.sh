a2062a509ff358d657c51c0e28fd705b8a6192a8
Jannik Brinkmann <jannik.brinkmann698@gmail.com>
2024-02-23 10:58:08 +0100

    delete unused files

--- run.sh
+++
@@ -1,6 +0,0 @@
-export CUDA_DEVICE_ORDER=PCI_BUS_ID
-export CUDA_VISIBLE_DEVICES=2
-export TRANSFORMERS_CACHE=/ceph/jbrinkma/cache/transformers
-export HF_DATASETS_CACHE=/ceph/jbrinkma/cache/datasets
-
-python3 run.py
