5f274ee88d5d9781228027fd45afc40316839d24
Jannik Brinkmann <jannik.brinkmann698@gmail.com>
2024-02-23 11:11:57 +0100

    replace optimizer

--- src/training/optimizer.py
+++ src/training/optimizer.py
@@ -1,27 +1,39 @@
 import torch
 
-
-class ConstrainedAdamW(torch.optim.AdamW):
+class ConstrainedAdamW(torch.optim.Adam):
     """
-    Variation of AdamW where some of the parameters are constrained to have unit norm.
-    
-    Copied from Sam Marks
+    A variant of Adam where some of the parameters are constrained to have unit norm.
+    From Sam Marks
     """
     def __init__(self, params, constrained_param, lr):
         super().__init__(params, lr=lr)
         self.constrained_param = constrained_param
     
     def step(self, closure=None):
-        
-        # project away the parallel component of the gradient
         with torch.no_grad():
             p = self.constrained_param
-            normed_p = p / p.norm(dim=-1, keepdim=True)
-            p.grad -= (p.grad * normed_p).sum(dim=-1, keepdim=True) * normed_p
+            ## Joseph implementation
+            # parallel_component = einops.einsum(
+            #     p.grad,    
+            #     p.data,
+            #     # "d_sae d_in, d_sae d_in -> d_sae",
+            #     "d_in d_sae , d_in d_sae  -> d_sae",
+            # )
+
+            # p.grad -= einops.einsum(
+            #     parallel_component,
+            #     p.data,
+            #     # "d_sae, d_sae d_in -> d_sae d_in",
+            #     "d_sae, d_in d_sae  ->  d_in d_sae",
+            # )
         
+            # Sam Marks implementation
+            normed_p = p / p.norm(dim=0, keepdim=True)
+            # project away the parallel component of the gradient
+            p.grad -= (p.grad * normed_p).sum(dim=0, keepdim=True) * normed_p
         super().step(closure=closure)
-        
-        # renormalize the constrained parameters
         with torch.no_grad():
             p = self.constrained_param
-            p /= p.norm(dim=-1, keepdim=True)
+
+            # renormalize the constrained parameters
+            p /= p.norm(dim=0, keepdim=True)
